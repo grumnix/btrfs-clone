626d0d4cee637fe975fd2cfbc6fc54afed2a3510
Ingo Ruhnke <grumbel@gmail.com>
2023-01-23 18:44:26 +0100

    Add Nix flake and setup.py

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonPackages = pkgs.python3Packages;
+      in {
+        packages = rec {
+          default = btrfs-clone;
+
+          btrfs-clone = pythonPackages.buildPythonPackage rec {
+            pname = "btrfs-clone";
+            version = "0.0.0";
+
+            src = ./.;
+          };
+        };
+      }
+    );
+}
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='btrfs-clone',
+      version='0.0.0',
+      scripts = ['btrfs-clone'])
+
+# EOF #
